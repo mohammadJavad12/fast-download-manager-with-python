e077b939ad08adfa20fc2e22ad64cd61845817eb
mohammadJavad12 <80960346+mohammadJavad12@users.noreply.github.com>
2021-03-19 16:07:48 +0330

    Add files via upload

---
+++ Main.py
@@ -0,0 +1,21 @@
+import requests
+from tkinter import *
+def download():
+    DT = DE.get()
+    FT = FE.get()
+    with open( FT,"wb") as f:
+        f.write(requests.get(DT).content)
+
+
+win = Tk()
+DL = Label(win, text="download link")
+DL.pack()
+DE = Entry(win)
+DE.pack()
+FS = Label(win, text="filename(with format)")
+FS.pack()
+FE = Entry(win)
+FE.pack()
+DB = Button(win, text="download" , command = download)
+DB.pack()
+win.mainloop()
